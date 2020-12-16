96db8b50950cc6960d57519e53e80c4e206c814b
Mufidzz <dmzmzzz@gmail.com>
2020-12-16 14:03:17 +0700

    Init

---
+++ README.md
@@ -0,0 +1,84 @@
+# iflabapi
+
+Flutter Package for Accessing Infotech API
+
+## Getting Started
+
+This project is a starting point for a Dart
+[package](https://flutter.dev/developing-packages/),
+a library module containing code that can be shared easily across
+multiple Flutter or Dart projects.
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.dev/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
+
+## Installing
+```
+dependencies:
+  http: ^0.12.2
+  flutter_secure_storage: ^3.3.5
+```
+
+## Authenticating
+
+```
+Auth auth = new Auth(
+    client: "your_client_id", 
+    secret: "your_client_secreet", 
+    username: "infotech_ilab_username", 
+    password: "infotech_ilab_password"
+  );
+
+isAuthorize = await auth.authorize();
+
+if (isAuthorize) {
+  //do what you want here if authorized 
+}
+```
+
+## Auth Message
+if you get rejected handshake while authenticating you can show the response message with
+
+```
+auth.getMessage();
+```
+
+## Secure Request
+you can do secure request after do authorization, you dont need to pass auth class to secure storage, just feel free to using method of this class
+
+```
+SecureRequest sr = new SecureRequest();
+
+body = json.encode({
+  "User" : "A"
+})
+
+sr.get(["user", "3"]);
+sr.post(["a"], body: body);
+
+```
+
+or if you need to change URLs of API Resource feel free to use URL parameter
+
+```
+SecureRequest sr = new SecureRequest(url: "http://new.url.to.resource");
+```
+
+## Parsing Data
+for parsing data from resource server just use 
+```
+var myData = await sr.get(["user", "17"]);
+```
+get Response Meta
+```
+print(myData.Meta["Key"])
+//or print all available key
+print(myData.getMetaKey)
+```
+get Response Data
+```
+print(myData.Data["Key"])
+//or print all available key
+print(myData.getDataKey)
+```
---
+++ lib/iflabapi.dart
@@ -0,0 +1,193 @@
+library iflabapi;
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureRequestResponse {
+  // ignore: non_constant_identifier_names
+  Map<String, dynamic> Meta;
+
+  // ignore: non_constant_identifier_names
+  Map<String, dynamic> Data;
+  int statusCode;
+
+  SecureRequestResponse({meta, data, sc}) {
+    Meta = meta;
+    Data = data;
+    statusCode = sc;
+  }
+
+  Iterable<String> getMetaKey() {
+    return Meta.keys;
+  }
+
+  Iterable<String> getDataKey() {
+    return Data.keys;
+  }
+}
+
+///This class is purposed to Make Request using
+///Secure Auth From Auth Class, you must do Auth before using this
+class SecureRequest {
+  String _resourceURL;
+  final _sk = '__api-auth-q';
+  final _storage = new FlutterSecureStorage();
+
+  SecureRequest({String url = 'http://api.infotech.umm.ac.id/api/v1/'}) {
+    _resourceURL = url;
+  }
+
+  ///Secure HTTP Request to our Endpoint, use method(["path1", "path2"])
+  ///instead of making new variable
+  Future<SecureRequestResponse> get(List<String> path) async {
+    var response = await http.get(_resourceURL + parsePath(path),
+        headers: {'cookie': await _storage.read(key: _sk)});
+
+    return _parseResponse(response);
+  }
+
+  ///Secure HTTP Request to our Endpoint, use method(["path1", "path2"])
+  ///instead of making new variable
+  Future<SecureRequestResponse> post(List<String> path, {body}) async {
+    var response = await http.post(_resourceURL + parsePath(path),
+        body: body, headers: {'cookie': await _storage.read(key: _sk)});
+
+    return _parseResponse(response);
+  }
+
+  ///Secure HTTP Request to our Endpoint, use method(["path1", "path2"])
+  ///instead of making new variable
+  Future<SecureRequestResponse> put(List<String> path, {body}) async {
+    var response = await http.put(_resourceURL + parsePath(path),
+        body: body, headers: {'cookie': await _storage.read(key: _sk)});
+
+    return _parseResponse(response);
+  }
+
+  ///Secure HTTP Request to our Endpoint, use method(["path1", "path2"])
+  ///instead of making new variable
+  Future<SecureRequestResponse> delete(List<String> path, {body}) async {
+    var response = await http.delete(_resourceURL + parsePath(path),
+        headers: {'cookie': await _storage.read(key: _sk)});
+
+    return _parseResponse(response);
+  }
+
+  String parsePath(List<String> path) {
+    String realPath = "";
+    path.forEach((element) {
+      realPath += element + "/";
+    });
+
+    return realPath;
+  }
+
+  SecureRequestResponse _parseResponse(http.Response response) {
+    var dec = json.decode(response.body);
+
+    return new SecureRequestResponse(
+      meta: dec["Meta"],
+      data: dec["Data"],
+      sc: response.statusCode,
+    );
+  }
+}
+
+///Auth Class purposed to Get Auth-Token which can used for All Infotech API
+///you can change the URL for make debug or hit to demo Infotech API URLs
+class Auth {
+  String _client;
+  String _clientSecret;
+  String _username;
+  String _password;
+  String _url;
+
+  String _message;
+
+  final _sk = '__api-auth-q';
+  final _storage = new FlutterSecureStorage();
+
+  Auth({
+    @required String client,
+    @required String secret,
+    @required String username,
+    @required String password,
+    String url = 'http://api.infotech.umm.ac.id/auth/',
+  }) {
+    _client = client;
+    _clientSecret = secret;
+    _username = username;
+    _password = password;
+    _url = url;
+  }
+
+  ///This method will return false if no user logged and vice versa,
+  ///you can parse message for make sure is user present or not
+  Future<bool> checkAuth() async {
+    String value = await _storage.read(key: _sk);
+    _message = "user credential is not present, please re-auth";
+    if (value.isEmpty) {
+      return false;
+    }
+
+    _message = "user credential is present";
+    return true;
+  }
+
+  ///Method for get Auth-Token and Save to Secure Storage
+  ///you can call this method after go with Auth Constructor
+  ///This will return true if authorized and vice versa
+  Future<bool> authorize() async {
+    var response = await http.post(
+      _url,
+      body: {
+        "UserName": _username,
+        "Password": _password,
+      },
+      headers: {
+        "IFX-CLIENT": _client,
+        "IFX-SECRET": _clientSecret,
+      },
+    );
+
+    _message = response.body;
+    if (response.statusCode != 200) {
+      return false;
+    }
+
+    if (response.headers['set-cookie'].isNotEmpty) {
+      final storage = new FlutterSecureStorage();
+      await storage.write(
+        key: _sk,
+        value: _parseToRequestCookie(response),
+      );
+    }
+
+    return true;
+  }
+
+  ///Method for parsing set-cookie response header from server and
+  ///serve to cookie at request header
+  String _parseToRequestCookie(http.Response response) {
+    String rawCookie = response.headers['set-cookie'];
+    var c = rawCookie.split(",");
+    var s = "";
+
+    c.forEach((el) {
+      if (el != null) {
+        int index = rawCookie.indexOf(';');
+        s += (index == -1) ? el : el.substring(0, index) + ";";
+      }
+    });
+
+    return s;
+  }
+
+  ///Method for get Message from Server after Request
+  String getMessage() {
+    return _message;
+  }
+}
